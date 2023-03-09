2e743293d9195e4744740db570d51b5c03befbdd
shavkatovivich <frontend6774@gmail.com>
2023-03-09 23:53:20 +0000

    Add tailwind css and logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,9 @@ const Navbar = () => {
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link to={"/"} className="flex items-center gap-2" onClick={() => { setActive (""); window.scrollTo(0,0)}}>
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+          <p className='text-white text-[18px] font-bold cursor pointer'>Ziyovuddin <span className='sm:block hidden'>|Frontend Master</span></p>
         </Link>
+        <p className="text-red-500">fjkewgjflh</p>
       </div>
     </nav>
   )
