2d8fc6f3d875f5c47b92d35a03071f4c80b5dd4f
shavkatovivich <frontend6774@gmail.com>
2023-03-10 01:49:05 +0000

    Add navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,48 @@
-import React, {useEffect, useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-import {styles} from "../style"
-import {navLinks} from "../constants"
-import {logo, menu, close} from "../assets"
+import { styles } from "../style";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return (
-    <nav className={`${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link to={"/"} className="flex items-center gap-2" onClick={() => { setActive (""); window.scrollTo(0,0)}}>
-          <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor pointer'>Ziyovuddin <span className='sm:block hidden'>|Frontend Master</span></p>
-        </Link>
-        <p className="text-red-500">fjkewgjflh</p>
-      </div>
-    </nav>
-  )
-}
+    const [active, setActive] = useState();
 
-export default Navbar
+    return (
+        <nav
+            className={`${styles.paddingX} w-full flex justify-between item-center py-5 fixed top-0 z-20 bg-primary`}
+        >
+            <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+                <Link
+                    to={"/"}
+                    className="flex items-center gap-2"
+                    onClick={() => {
+                        setActive("");
+                        window.scrollTo(0, 0);
+                    }}
+                >
+                    <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+                    <p className="text-white text-[18px] font-bold cursor pointer">
+                        Ziyovuddin <span className="sm:block hidden">|Frontend Master</span>
+                    </p>
+                </Link>
+
+                <ul className="list-none hidden sm:flex flex-grow gap-10">
+                    {navLinks.map((link) => (
+                        <li
+                            className={`${active === link.title ? "text-white" : "text-secondary"
+                                } hover:text-white text-[18px] font-medium cursor-pointer`}
+                            onClick={() => setActive(link.title)}
+                            key={link.id}
+                        >
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+                
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
