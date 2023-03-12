4e82b2db334c32bc01b647ad4d70e327995c37d0
shavkatovivich <frontend6774@gmail.com>
2023-03-12 02:19:07 +0000

    Add computer img files

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,6 +6,7 @@ import CanvasLoader from "../Loader";
 
 const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
+  console.log(computer.scene);
   return (
       <mesh>
         <hemisphereLight intensity={0.15} groundColor="black" />
